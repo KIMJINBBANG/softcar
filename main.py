97742d61465f12a7b786e9acfce1bbd279d75ae7
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-02-21 23:28:13 +0900

    still_trying

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return render_template('index.html')
     return render_template('index.html')
 
+@app.route('/')
+def second():
+    return render_template('indexAndMap.html')
+
+
 def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        eyes_csd_sec = camera.closed_sec
-        if eyes_csd_sec >= 3:
-            call_phone()
+    # while True:
+    #     frame = camera.get_frame()
+    #     eyes_csd_sec = camera.closed_sec
+        # if eyes_csd_sec >= 3:
+        call_phone()
             # print("hello")
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        # yield (b'--frame\r\n'
+        #        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
@@ -26,12 +30,19 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+# def findmap():
+#     return redirect(url_for('index'))
+
 """if camera spot driver is now sleeping, call the phone and give the safe destination location data."""
 # @app.route('/api/data')
 def call_phone():
     api_endpoint = "https://tl9b7wjalf.execute-api.ap-northeast-2.amazonaws.com/default/helloc"
     response = requests.get(api_endpoint)
-    map()
+    # findmap()
+
+    requests.get('http://127.0.0.1:5000')
+
+    # requests.get('/new_page')
 
     if response.status_code == 200:
         data = response.json()
@@ -46,13 +57,13 @@ if __name__ == '__main__':
 #     # return render_template('indexAndMap.html')
 #     return redirect('indexAndMap.html')
 
-@app.route('/newpage')
-def new_page():
-    # return render_template('index.html')
-    return render_template('indexAndMap.html')
+# @app.route('/new_page')
+# def new_page():
+#     # return render_template('index.html')
+#     return render_template('indexAndMap.html')
 
-def map():
-    return redirect(url_for('newpage'))
+# @app.route('/redirect_to_newpage')
+
 
 
 # @app.route('/indexAndMap.html', methods=['GET'])
