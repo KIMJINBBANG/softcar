c2ff3e1209f963b9a3533de5708eafaa83c21a10
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-02-22 23:46:40 +0900

    final

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, Response, redirect, url_for
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
 from camera import VideoCamera
 import requests
 
 app = Flask(__name__)
 called = False
 
-driver = webdriver.Chrome("C:/Users/김진영/Downloads/chromedriver_win32")
-url = "http://127.0.0.1:5000/"
-driver.get(url)
-driver.implicitly_wait(1)
+# driver = webdriver.Chrome("C:/Users/김진영/Downloads/chromedriver_win32")
+# url = "http://127.0.0.1:5000"
+# driver.get(url)
 
 @app.route('/')
 def index():
@@ -32,20 +29,14 @@ def index():
     else:
         return render_template('indexAndMap.html')
 
-# @app.route('/')
-# def second():
-#     return render_template('indexAndMap.html')
-
-
 def gen(camera):
-    # while True:
-        # frame = camera.get_frame()
-        # eyes_csd_sec = camera.closed_sec
-        # if eyes_csd_sec >= 3:
+    while True:
+        frame = camera.get_frame()
+        eyes_csd_sec = camera.closed_sec
+        if eyes_csd_sec >= 3:
             call_phone()
-            # print("hello")
-        # yield (b'--frame\r\n'
-        #        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
@@ -59,15 +50,11 @@ def findmap():
 
 """if camera spot driver is now sleeping, call the phone and give the safe destination location data."""
 # @app.route('/api/data')
+
 def call_phone():
     api_endpoint = "https://tl9b7wjalf.execute-api.ap-northeast-2.amazonaws.com/default/helloc"
     response = requests.get(api_endpoint)
     findmap()
-
-    # requests.get('')
-
-
-    # requests.get('/new_page')
 
     if response.status_code == 200:
         data = response.json()
@@ -77,29 +64,3 @@ def call_phone():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
-# def map():
-#     # return render_template('indexAndMap.html')
-#     return redirect('indexAndMap.html')
-
-# @app.route('/new_page')
-# def new_page():
-#     # return render_template('index.html')
-#     return render_template('indexAndMap.html')
-
-# @app.route('/redirect_to_newpage')
-
-
-
-# @app.route('/indexAndMap.html', methods=['GET'])
-# def make_pin():
-#     if request.method == 'GET':
-#         # 버튼 클릭 이벤트 발생시
-#         # HTML 템플릿 렌더링
-#         return render_template('index.html')
-#     else:
-#         return render_template('index.html')
-#     return 'Hello, World!'
-
-# if __name__ == '__main__':
-#     app.run()
